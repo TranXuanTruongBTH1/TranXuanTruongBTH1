aa77c9b2a72f81b97b1c591a5f989a413510b4cc
TXT635 <atruong0306@gmail.com>
2023-03-10 05:13:24 -0800

    commit note

---
+++ BAITHUCHANH0703/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BAITHUCHANH0703.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
