8ee5f4225f4d954eeda16846c899b23dfcd1e700
TXT635 <atruong0306@gmail.com>
2023-03-18 23:40:02 -0700

    commit note

--- BAITHUCHANH0703/Controllers/studentController.cs
+++ BAITHUCHANH0703/Controllers/studentController.cs
@@ -29,7 +29,6 @@ public IActionResult Index()
      [HttpPost]
     public IActionResult Tinhtong(string Number)
     {
-        //Chuyen đoi kieu du lieu
         int so = Convert.ToInt32(Number);
         int tong = 0;
         while(so >0)
@@ -45,7 +44,6 @@ public IActionResult Index()
         return View();
     }
  [HttpPost]
-        //giải phương trình b2
         public string giaiphuongtrinhbac2(double a, double b, double c)
         {
             string thongBao = "";
@@ -57,12 +55,10 @@ public IActionResult Index()
                 thongBao = GiaiPhuongTrinhBacNhat(b, c);
                 if (b == 0)
                 {
-                    //nếu b = 0 và c = 0 thì phương trình vô số nghiệm
                     if (c == 0)
                     {
                         thongBao = " Phuong trinh  vo so   nghiem.";
                     }
-                    //nếu b = 0 và c != 0 thì phương trình vô nghiệm
                     else
                     {
                         thongBao = "Phuong trinh  vo  nghiem.";
@@ -71,28 +67,22 @@ public IActionResult Index()
                 else
                 {
                     x1 = (double)-c / b;
-                    //Sử dụng phương thứ Math.Round() để làm tròn kết quả lên 2 số thập phân
                     thongBao = "Phuong trinh co nghiem duy nhat x = {0}" + (Math.Round(x1, 2));
                 }
             
             }
-            //nếu a != 0 thì ta bắt đầu giải phương trình bậc hai
             else
             {
-                //tính delta
                 delta = Math.Pow(b, 2) - 4 * a * c;
-                //kiểm tra nếu delta < 0 thì phương trình vô nghiệm
                 if (delta < 0)
                 {
                     thongBao = "Phuong trinh vo nghiem.";
                 }
-                //nếu delta = 0 thì phương trình có hai nghiệm kép
                 else if (delta == 0)
                 {
                     x1 = x2 = -b / (2 * a);
                     thongBao = "\n Phuong trinh co nghiem kep x1 = x2 = {0}" + x1;
                 }
-                //nếu delta > 0 thì phuong trình có hai nghiệm phân biệt
                 else
                 {
                     x1 = (-b + Math.Sqrt(delta)) / (2 * a);
@@ -104,14 +94,10 @@ public IActionResult Index()
 
             return thongBao;
         }
-        //xay dung cac phuong thuc
         public string GiaiPhuongTrinhBacNhat(double a, double b)
         {
-            //khai bao 1 bien  de chua du lieu
             string thongBao = "";
-            //khai bao 1 bien so de chua gia tri
             double x;
-            //Giai phuong trinh
             if (a == 0)
             {
                 if (b== 0)
@@ -126,11 +112,9 @@ public IActionResult Index()
             }
             else
             {
-                //tinh nghiem cua phuong trinh
                 x = -b / a;
                 thongBao = " nghiem kep x= " + x;
             }
-            //tra ve du lieu
             return thongBao;
         }
     }
